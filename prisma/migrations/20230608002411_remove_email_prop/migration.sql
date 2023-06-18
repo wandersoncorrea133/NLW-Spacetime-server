35024e3dd567cd7b64338e756204fc3613f031c5
wandersondacosta <wanderson_c_c@hotmail.com>
2023-06-18 08:46:35 -0300

    create route login at app

---
+++ prisma/migrations/20230608002411_remove_email_prop/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "githubId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "avatarURL" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("avatarURL", "githubId", "id", "login", "name") SELECT "avatarURL", "githubId", "id", "login", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
